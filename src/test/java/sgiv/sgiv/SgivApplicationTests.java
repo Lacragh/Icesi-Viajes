42911745c9fbf2e3b4bea6d288ba95fe4b18d45e
Victor Manuel Garzon Meneses <panterrosa90@gmail.com>
2024-04-20 22:34:06 -0500

    Estructura

---
+++ src/test/java/sgiv/sgiv/SgivApplicationTests.java
@@ -0,0 +1,13 @@
+package sgiv.sgiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
