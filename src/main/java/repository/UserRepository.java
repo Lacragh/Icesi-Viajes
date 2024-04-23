64785e93bca1776899a124680666113dc4486d45
Victor Manuel Garzon Meneses <panterrosa90@gmail.com>
2024-04-23 10:33:51 -0500

    feat(login): add structure login

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -3,7 +3,16 @@ package repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import domain.User;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@EnableJpaRepositories
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+    Optional<User> findByLoginAndPassword(String login, String password);
+
+    User findByLogin(String email);
 }
--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -3,11 +3,15 @@ package service;
 import java.util.List;
 import java.util.Optional;
 
+import dto.UserDTO;
+import dto.loginDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import domain.User;
+import payload.response.LoginResponse;
 import repository.UserRepository;
 
 @Scope("singleton")
@@ -16,6 +20,9 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	public List<User> findAll() {
@@ -65,6 +72,42 @@ public class UserServiceImpl implements UserService{
 	public Long count() {
 		// TODO Auto-generated method stub
 		return userRepository.count();
+	}
+
+	@Override
+	public String addUser(UserDTO userDTO) {
+		User user = new User(
+				userDTO.getId_user(),
+				userDTO.getLogin(),
+				this.passwordEncoder.encode(userDTO.getPassword()),
+				userDTO.getFirst_name(),
+				userDTO.getLast_name()
+		);
+		userRepository.save(user);
+		return user.getFirst_name();
+	}
+
+	@Override
+	public LoginResponse loginUser(loginDTO loginDTO) {
+		String msg = "";
+		User user = userRepository.findByLogin(loginDTO.getEmail());
+		if (user != null){
+			String password = loginDTO.getPassword();
+			String encodePassword = user.getPassword();
+			Boolean isPwdRight = passwordEncoder.matches(password, encodePassword);
+			if (isPwdRight){
+				Optional<User> user1 = userRepository.findByLoginAndPassword(loginDTO.getEmail(),loginDTO.getPassword());
+				if(user1.isPresent()) {
+					return new LoginResponse("Login Success", true);
+				}else{
+					return new LoginResponse("Login Failed", false);
+				}
+			}else{
+				return new LoginResponse("Password Not Match", false);
+			}
+		}else{
+			return new LoginResponse("Email not exist", false);
+		}
 	}
 
 }
